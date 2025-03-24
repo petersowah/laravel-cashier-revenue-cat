271c0bff7631d867090150f0fc7b2c4ec61a5ae4
Nii Adjetey Sowah <petersowah@gmail.com>
2025-03-24 06:35:19 +0000

    chore: update dependencies for Laravel 12 and simplify arch test

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,5 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+test('package class exists', function () {
+    expect(class_exists('PeterSowah\LaravelCashierRevenueCat\LaravelCashierRevenueCatServiceProvider'))->toBeTrue();
+});
