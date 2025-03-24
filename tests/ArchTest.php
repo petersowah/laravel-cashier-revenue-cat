9df3546ffba2e54322917038ba0e02a7526ff2e7
Nii Adjetey Sowah <petersowah@gmail.com>
2025-03-24 06:43:54 +0000

    clean

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -3,3 +3,7 @@
 test('package class exists', function () {
     expect(class_exists('PeterSowah\LaravelCashierRevenueCat\LaravelCashierRevenueCatServiceProvider'))->toBeTrue();
 });
+
+arch('it will not use debugging functions')
+    ->expect(['dd', 'dump', 'ray'])
+    ->each->not->toBeUsed();
