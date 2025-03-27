4173be6a906f426f3ce354fceee4603f60f9aa74
Nii Adjetey Sowah <petersowah@gmail.com>
2025-03-27 15:55:58 +0000

    clean

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -2,6 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 use PeterSowah\LaravelCashierRevenueCat\Http\Controllers\WebhookController;
-
-Route::post('revenuecat/webhook', [WebhookController::class, 'handleWebhook'])
-    ->name('cashier.webhook');
